294bd32c7ce667abf8f9a2d8190827f6f574f263
berkagmpp <berkagmpp@gmail.com>
2022-06-17 13:02:05 +1200

    Feat: Add contextAPI fn in auth-context.js Add AuthContext using createContext(react) Add AuthContextProvider using props, useState(react) to pass the token is taken from the API

---
+++ src/store/auth-context.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const AuthContext = React.createContext({
+    token: '',
+    isLoggedIn: false,
+    login: (token) => {},
+    logout: () => {},
+
+});
+
+export const AuthContextProvider = (props) => {
+    const [token, setToken] = useState(null);
+
+    const userIsLoggedIn = !!token;     // simply converts this truthy or falsy value
+    // if token is true and string, return true
+
+    const loginHandler = (token) => {
+        setToken(token);
+    };
+
+    const logoutHandler = () => {
+        setToken(null);
+    };
+
+    const contextValue = {
+        token: token,
+        isLoggedIn: userIsLoggedIn,
+        login: loginHandler,
+        logout: logoutHandler
+    };
+
+    return (
+        <AuthContextProvider value={contextValue}>
+            {props.children}
+        </AuthContextProvider>
+    );
+}
+
+export default AuthContext;
