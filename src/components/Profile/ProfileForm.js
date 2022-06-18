cd3daa272bb9e194790ecdc2d80608ae60129877
berkagmpp <berkagmpp@gmail.com>
2022-06-18 23:02:34 +1200

    Feat: Add submitHandler for new password in ProfileForm.js Add newPasswordInputRef using useRef(react) Add submitHandler fn for form Add fetch fn to send a HTTP request (POST) using useContext(react) and AuthContext

--- src/components/Profile/ProfileForm.js
+++ src/components/Profile/ProfileForm.js
@@ -1,17 +1,45 @@
+import { useRef, useContext } from 'react';
+
+import AuthContext from '../../store/auth-context';
+
 import classes from './ProfileForm.module.css';
 
 const ProfileForm = () => {
-  return (
-    <form className={classes.form}>
-      <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
-      </div>
-      <div className={classes.action}>
-        <button>Change Password</button>
-      </div>
-    </form>
-  );
+    const newPasswordInputRef = useRef();
+
+    const AuthCtx = useContext(AuthContext);
+
+    const submitHandler = event => {
+        event.preventDefault();
+
+        const enteredNewPassword = newPasswordInputRef.current.value;
+
+        // add validation
+
+        fetch('https://identitytoolkit.googleapis.com/v1/accounts:update?key=AIzaSyB2zalKGx016HSuDDm0EnL9zonlpy3_uV0', {
+            method: 'POST',
+            body: JSON.stringify({
+                idToken,
+                password,
+                returnSecureToken
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+    };
+
+    return (
+        <form className={classes.form} onSubmit={submitHandler}>
+            <div className={classes.control}>
+                <label htmlFor='new-password'>New Password</label>
+                <input type='password' id='new-password' ref={newPasswordInputRef} />
+            </div>
+            <div className={classes.action}>
+                <button>Change Password</button>
+            </div>
+        </form>
+    );
 }
 
 export default ProfileForm;
