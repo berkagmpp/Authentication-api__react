c5f92f44dfcb0e73674818186dd4710a95baf11b
berkagmpp <berkagmpp@gmail.com>
2022-06-20 10:48:00 +1200

    Feat: Add conditions to login to cannot visit auth and profile page Add isLoggedIn condition from AuthContext using useContext(react) to Route path of auth and profile in App.js (for preventing the user cannot approach both pages without authentication)

--- src/App.js
+++ src/App.js
@@ -1,26 +1,35 @@
+import { useContext } from 'react';
+
 import { Switch, Route } from 'react-router-dom';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
 import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
+import AuthContext from './store/auth-context';
 
 function App() {
-  return (
-    <Layout>
-      <Switch>
-        <Route path='/' exact>
-          <HomePage />
-        </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
-        <Route path='/profile'>
-          <UserProfile />
-        </Route>
-      </Switch>
-    </Layout>
-  );
+    const AuthCtx = useContext(AuthContext);
+
+    return (
+        <Layout>
+            <Switch>
+                <Route path='/' exact>
+                    <HomePage />
+                </Route>
+                {!AuthCtx.isLoggedIn && (
+                    <Route path='/auth'>
+                        <AuthPage />
+                    </Route>
+                )}
+                {AuthCtx.isLoggedIn && (
+                    <Route path='/profile'>
+                        <UserProfile />
+                    </Route>
+                )}
+            </Switch>
+        </Layout>
+    );
 }
 
 export default App;
