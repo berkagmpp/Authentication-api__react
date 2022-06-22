5670900b7af9d18504fb087a48f11d99147ab1e6
berkagmpp <berkagmpp@gmail.com>
2022-06-23 05:32:14 +1200

    Feat: Add remove token and expirationTime fn for the user manual logout case in auth-context.js Change to global var of logoutTimer Add condition for clearTimeout fn with logoutTimer in loginHandler fn Add const for storing data(token, expirationTime) in retrieveStoredToken fn Add condition for removing stored data Add return value as an obj to send AuthContextProvider fn Add const, let for initailToken from retrieveStoredToken fn Add localStorage fn for expirationTime in logoutHandler, loginHandler fn Add set logoutTimer with condition of stored data using useEffect(react) Add useCallback(react) to logoutHandler fn for storing fn Ref: #c044cd3259c38307f2aba916b956443083b91ebe Ref: #15dbe889c69857fe4d7c2287330ed5b300057917

--- src/store/auth-context.js
+++ src/store/auth-context.js
@@ -1,4 +1,6 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useCallback } from "react";
+
+let logoutTimer;
 
 const AuthContext = React.createContext({
     token: '',
@@ -17,26 +19,63 @@ const calculateRemainingTime = (expirationTime) => {
     return remainingDuration;
 };
 
+const retrieveStoredToken = () => {
+    const storedToken = localStorage.getItem('token');
+    const storedExpirationDate = localStorage.getItem('expirationTime');
+
+    const remainingTime = calculateRemainingTime(storedExpirationDate);
+
+    if (remainingTime <= 60000) {   // if remainingTime is less then 60 seconds
+        localStorage.removeItem('token');
+        localStorage.removeItem('expirationTime');
+        return null;
+    };
+
+    return {
+        token: storedToken,
+        duration: remainingTime
+    };
+};
+
 export const AuthContextProvider = (props) => {
-    const initialToken = localStorage.getItem('token');
+    const tokenData = retrieveStoredToken();
+    let initialToken;
+    if (tokenData) {
+        initialToken = tokenData.token;
+    };
+
     const [token, setToken] = useState(initialToken);
 
     const userIsLoggedIn = !!token;     // simply converts this truthy or falsy value
     // if token is true and string, return true
 
-    const logoutHandler = () => {
+    const logoutHandler = useCallback(() => {
         setToken(null);
         localStorage.removeItem('token');
-    };
+        localStorage.removeItem('expirationTime');
+
+        if (logoutTimer) {
+            clearTimeout(logoutTimer);
+        };
+    }, []);     // don't need to add dependency here because :
+    // 1. localStorage, clearTimeout is browser built-in fn, not specific
+    // 2. setToken is state updating fn, so never change 
+    // 3. logoutTimer is a global variable (outside of React rendering flow)
 
     const loginHandler = (token, expirationTime) => {
         setToken(token);
         localStorage.setItem('token', token);
+        localStorage.setItem('expirationTime', expirationTime);
 
         const remainingTime = calculateRemainingTime(expirationTime);
-
-        setTimeout(logoutHandler, remainingTime);
+        logoutTimer = setTimeout(logoutHandler, remainingTime);
     };
+
+    useEffect(() => {
+        if (tokenData) {
+            logoutTimer = setTimeout(logoutHandler, tokenData.duration);
+        }
+    }, [tokenData, logoutHandler]);
 
     const contextValue = {
         token: token,
